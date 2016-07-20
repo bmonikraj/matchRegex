df68ddd5667bd0636f9c382b11df3a94cb06b21d
MONIK RAJ BEHERA <bmonikraj@gmail.com>
2016-07-20 16:15:46 +0000

    Update urls.py

--- matchRegex/regexApp/urls.py
+++ matchRegex/regexApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 url(r'^$', views.index, name='index'),
 url(r'^regexService/$', views.regexService, name='regexService'),
+url(r'^session/$', views.session, name='session'),
 ]
